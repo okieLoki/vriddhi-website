0b84435b15d54de0e1de7ea922d0054f586d0d63
Uddeepta Raaj Kashyap <uddeeptaraajkashyap@gmail.com>
2023-08-17 22:41:27 +0530

    download link updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,24 +20,26 @@ const Navbar = () => {
                         <Link to='home' smooth='true' duration={500}>Home</Link>
                     </li>
                     <li>
-                    <Link to='about' smooth='true' duration={500}>About</Link>
+                        <Link to='about' smooth='true' duration={500}>About</Link>
                     </li>
                     <li>
-                    <Link to='articles' smooth='true' duration={500}>Articles</Link>
+                        <Link to='articles' smooth='true' duration={500}>Articles</Link>
                     </li>
                     <li>
-                    <Link to='contact' smooth='true' duration={500}>Contact</Link>
+                        <Link to='contact' smooth='true' duration={500}>Contact</Link>
                     </li>
                     <li>
-                    <Link to='graphs' smooth='true' duration={500}>Stats</Link>
+                        <Link to='graphs' smooth='true' duration={500}>Stats</Link>
                     </li>
                     <li>
-                    <Link to='testimonials' smooth='true' duration={500}>Testimonials</Link>
+                        <Link to='testimonials' smooth='true' duration={500}>Testimonials</Link>
                     </li>
                 </ul>
 
                 <div className='btn-group'>
-                    <button className='btn'>Download</button>
+                    <a href="https://drive.google.com/file/d/1RC5u3Aynp9JMRqKusNKcGL3L-OWi0y2G/view?usp=sharing">
+                        <button className='btn'>Download</button>
+                    </a>
                 </div>
 
                 <div className='hamburger' onClick={handleClick}>
